6317798dc7ff13e26338ea98feca065ec5d82874
actimeller <max.post@bk.ru>
2016-05-29 22:50:46 +0300

    intro

---
+++ src/components/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+//const SearchBar = () => {
+//    return <input />
+//};
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={this.onInputChange.bind(this)}/>
+
+            </div>
+        )
+    }
+
+    onInputChange(e) {
+        this.setState({
+            term: e.target.value
+        })
+    }
+}
+
+export default SearchBar
